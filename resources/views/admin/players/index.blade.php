5a9fff42f31f24f9f3660ac6b6d0ded51f4c63b7
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-11-02 16:13:33 +0700

    commit21

--- resources/views/admin/players/index.blade.php
+++ resources/views/admin/players/index.blade.php
@@ -89,14 +89,7 @@
                                 <td>2012/12/02</td>
                                 <td>$372,000</td>
                             </tr>
-                            <tr>
-                                <td>Herrod Chandler</td>
-                                <td>Sales Assistant</td>
-                                <td>San Francisco</td>
-                                <td>59</td>
-                                <td>2012/08/06</td>
-                                <td>$137,500</td>
-                            </tr>
+                            
                             <tr>
                                 <td>Rhona Davidson</td>
                                 <td>Integration Specialist</td>
