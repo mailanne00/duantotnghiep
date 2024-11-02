785f0d030caea05ad10bbefaf63168c5d0e97fe4
Vdoan2004 <doannvph35275@fpt.edu.vn>
2024-11-02 22:11:55 +0700

    1

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\BinhLuanController;
 use App\Http\Controllers\DangTinController;
+use App\Http\Controllers\DanhGiaController;
 use App\Http\Controllers\DanhMucController;
+use App\Http\Controllers\PhanQuyenController;
 use App\Http\Controllers\PhuongThucThanhToanController;
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
@@ -66,3 +68,16 @@ Route::get('/binhluans/{id}/edit', [BinhLuanController::class, 'edit'])->name('b
 Route::put('/binhluans/{id}', [BinhLuanController::class, 'update'])->name('binhluans.update');
 Route::put('/binhluans/{id}/update-status', [BinhLuanController::class, 'updateStatus'])->name('binhluans.update-status');
 Route::delete('/binhluans/{id}', [BinhLuanController::class, 'destroy'])->name('binhluans.destroy');
+
+
+Route::get('/phan-quyens', [PhanQuyenController::class, 'index'])->name('phanquyen.index');
+Route::get('/phan-quyens/create', [PhanQuyenController::class, 'create'])->name('phanquyen.create');
+Route::post('/phan-quyens/store', [PhanQuyenController::class, 'store'])->name('phanquyen.store');
+Route::get('/phan-quyens/edit/{id}', [PhanQuyenController::class, 'edit'])->name('phanquyen.edit');
+Route::put('/phan-quyens/update/{id}', [PhanQuyenController::class, 'update'])->name('phanquyen.update');
+Route::delete('/phan-quyens/{id}', [PhanQuyenController::class, 'destroy'])->name('phanquyen.destroy');
+
+
+Route::get('danh-gias', [DanhGiaController::class, 'index'])->name('danhgia.index');
+Route::get('/danh-gias/{id}', [DanhGiaController::class, 'getAverageRating'])->name('danhgia.show');
+Route::post('/danh-gias/store', [DanhGiaController::class, 'store'])->name('danhgia.store');
