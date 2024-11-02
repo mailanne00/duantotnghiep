9053435af15da7d8e4c7c49356952889a4c1cc2a
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-11-02 16:14:34 +0700

    commit23

--- resources/views/admin/players/index.blade.php
+++ resources/views/admin/players/index.blade.php
@@ -83,14 +83,6 @@
                             </tr>
                             
                             <tr>
-                                <td>Rhona Davidson</td>
-                                <td>Integration Specialist</td>
-                                <td>Tokyo</td>
-                                <td>55</td>
-                                <td>2010/10/14</td>
-                                <td>$327,900</td>
-                            </tr>
-                            <tr>
                                 <td>Colleen Hurst</td>
                                 <td>Javascript Developer</td>
                                 <td>San Francisco</td>
