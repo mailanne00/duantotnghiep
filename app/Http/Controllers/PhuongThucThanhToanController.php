95eeb38d0a18e6c40fa63038c1e72f7a9af70a2b
mailanne00 <lanvmph37426@fpt.edu.vn>
2024-11-02 16:13:02 +0700

    lan1

--- app/Http/Controllers/PhuongThucThanhToanController.php
+++ app/Http/Controllers/PhuongThucThanhToanController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PhuongThucThanhToanStoreRequest;
+use App\Models\PhuongThucThanhToan;
 use Illuminate\Http\Request;
 
 class PhuongThucThanhToanController extends Controller
 {
-    //
+    const PATH_UPLOAD = "public/logo";
+    public function index()
+    {
+        $phuongthucthanhtoans = PhuongThucThanhToan::all();
+        return view("admin.phuong-thuc-thanh-toans.index", compact('phuongthucthanhtoans'));
+    }
+
+    public function create()
+    {
+        return view('admin.phuong-thuc-thanh-toans.create');
+    }
+
+    public function store(PhuongThucThanhToanStoreRequest $request)
+    {
+        $validate = $request->validated();
+        $data = $request->except('logo');
+
+        if ($request->hasFile('logo')) {
+            $data['logo'] = Storage::put(self::PATH_UPLOAD, $request->file('logo'));
+        }
+        PhuongThucThanhToan::create($data);
+
+        return redirect()->route('admin.phuongthucthanhtoans.index');
+    }
+
+    public function edit(int $id)
+    {
+        $phuongthucthanhtoan = PhuongThucThanhToan::findOrFail($id);
+        return view('admin.phuong-thuc-thanh-toans.edit', compact('phuongthucthanhtoan'));
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $phuongthucthanhtoan = PhuongThucThanhToan::findOrFail($id);
+        $data = $request->except('logo');
+
+        if ($request->hasFile('logo')) {
+            Storage::disk('public')->delete($request->logo);
+            $data['logo'] = Storage::put(self::PATH_UPLOAD, $request->file('logo'));
+        }
+        $phuongthucthanhtoan->update($data);
+
+        return redirect()->route('admin.phuongthucthanhtoans.index');
+    }
+
+    public function updateStatus(Request $request, $id)
+    {
+        $phuongthucthanhtoan = PhuongThucThanhToan::findOrFail($id);
+        $trang_thai = $request->input('trang_thai') ? 1 : 0;
+        $phuongthucthanhtoan->trang_thai = $trang_thai;
+        $phuongthucthanhtoan->save();
+
+        return redirect()->route('admin.phuongthucthanhtoans.index')->with('success', 'Cập nhật thành công.');
+    }
+
+    public function destroy(PhuongThucThanhToan $id)
+    {
+        $phuongthucthanhtoan = PhuongThucThanhToan::findOrFail($id);
+        $phuongthucthanhtoan->delete();
+        return redirect()->route('admin.phuongthucthanhtoans.index');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DangTinController;
+use App\Http\Controllers\PhuongThucThanhToanController;
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,5 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.layouts.app');
 });
+
+Route::get('/dangtins', [DangTinController::class, 'index'])->name('dangtins.index');
+Route::get('/dangtins/create', [DangTinController::class, 'create'])->name('dangtins.create');
+Route::post('/dangtins', [DangTinController::class, 'store'])->name('dangtins.store');
+Route::delete('/dangtins/{id}', [DangTinController::class, 'store'])->name('dangtins.destroy');
+
+Route::get('/phuongthucthanhtoans', [PhuongThucThanhToanController::class, 'index'])->name('phuongthucthanhtoans.index');
+Route::get('/phuongthucthanhtoans/create', [PhuongThucThanhToanController::class, 'create'])->name('phuongthucthanhtoans.create');
+Route::post('/phuongthucthanhtoans', [PhuongThucThanhToanController::class, 'store'])->name('phuongthucthanhtoans.store');
+Route::get('/phuongthucthanhtoans/{id}/edit', [PhuongThucThanhToanController::class, 'edit'])->name('phuongthucthanhtoans.edit');
+Route::put('/phuongthucthanhtoans/{id}', [PhuongThucThanhToanController::class, 'update'])->name('phuongthucthanhtoans.update');
+Route::put('/phuongthucthanhtoans/{id}/update-status', [PhuongThucThanhToanController::class, 'updateStatus'])->name('phuongthucthanhtoans.update-status');
+Route::delete('/phuongthucthanhtoans/{id}', [PhuongThucThanhToanController::class, 'destroy'])->name('phuongthucthanhtoans.destroy');
+
 
 Route::resource('players', PlayerController::class);
