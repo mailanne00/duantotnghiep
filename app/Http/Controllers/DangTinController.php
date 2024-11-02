17a66ef3a4cc305a179ef296f98b72fb5914200a
mailanne00 <lanvmph37426@fpt.edu.vn>
2024-11-02 16:56:15 +0700

    lan5

--- app/Http/Controllers/DangTinController.php
+++ app/Http/Controllers/DangTinController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\DangTin;
 use App\Models\TaiKhoan;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
