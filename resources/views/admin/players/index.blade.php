c02591c8e111c145ae28c143b96b8e3a172d7e43
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-11-02 16:10:49 +0700

    commit16

--- resources/views/admin/players/index.blade.php
+++ resources/views/admin/players/index.blade.php
@@ -113,14 +113,6 @@
                                 <td>2009/09/15</td>
                                 <td>$205,500</td>
                             </tr>
-                            <tr>
-                                <td>Sonya Frost</td>
-                                <td>Software Engineer</td>
-                                <td>Edinburgh</td>
-                                <td>23</td>
-                                <td>2008/12/13</td>
-                                <td>$103,600</td>
-                            </tr>
                         </tbody>
                         <tfoot>
                             <tr>
