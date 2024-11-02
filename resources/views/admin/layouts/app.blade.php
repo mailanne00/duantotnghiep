390cb1f2f2018601457dbbf70760006a1fdca27a
mailanne00 <lanvmph37426@fpt.edu.vn>
2024-11-02 16:16:26 +0700

    lan2

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -85,6 +85,20 @@
                             <li class=""><a href="{{ route('admin.players.index') }}" class="">Danh sách</a></li>
                         </ul>
                     </li>
+                    <li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
+                        class="nav-item pcoded-hasmenu">
+                        <a href="#!" class="nav-link"><span class="pcoded-micon"><i
+                                    class="fas fa-user-secret"></i></span><span class="pcoded-mtext">Quản lý phương thức thanh toán</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="{{ route('admin.phuongthucthanhtoans.index') }}" class="">Danh sách</a></li>
+                            <li class=""><a href="{{ route('admin.phuongthucthanhtoans.index') }}" class="">Danh sách</a></li>
+                        </ul>
+                    </li>
+                    <li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
+                        class="nav-item pcoded-hasmenu">
+                        <a href="{{ route('admin.dangtins.index') }}" class="nav-link"><span class="pcoded-micon"><i
+                                    class="fas fa-user-secret"></i></span><span class="pcoded-mtext">Quản lý đăng tin</span></a>
+                    </li>
                 </ul>
             </div>
 
