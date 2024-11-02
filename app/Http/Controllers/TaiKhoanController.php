80f7d0e052647fc50c60e23690d77833eabc2223
djtmeconchoo11 <khangtbph39573@fpt.edu.vn>
2024-11-02 16:16:15 +0700

    list

--- app/Http/Controllers/TaiKhoanController.php
+++ app/Http/Controllers/TaiKhoanController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TaiKhoan;
 use Illuminate\Http\Request;
 
 class TaiKhoanController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $taikhoans = TaiKhoan::all();
+
+        return view('admin.taikhoans.index', compact('taikhoans'));
+    }
 }
