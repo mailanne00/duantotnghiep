16a5804f48a8c9096b91ad76c464164accd22c3f
Hoàng <hoang7bvo@gmail.com>
2024-11-02 15:54:20 +0700

    Hoang

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>@yield('title' , 'Đây là trang chủ')</title>
+    <title>@yield('title', 'Đây là trang chủ')</title>
     <meta name="description"
         content="Dasho Bootstrap admin template made using Bootstrap 5 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
     <meta name="keywords"
@@ -55,7 +55,8 @@
         <div class="navbar-wrapper ">
             <div class="navbar-brand header-logo" style="display: flex; align-items: center;">
                 <a href="index.html" class="b-brand">
-                    <img src="{{ asset('assets/images/logoPD.png') }}" alt="logo" style="margin-right: 5px; width: 30px">
+                    <img src="{{ asset('assets/images/logoPD.png') }}" alt="logo"
+                        style="margin-right: 5px; width: 30px">
                     <span style="color: #fff ;font-weight: bold; font-size: 16px;">PlayerDuo</span>
                 </a>
                 <a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
@@ -80,9 +81,39 @@
                     <li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
                         class="nav-item pcoded-hasmenu">
                         <a href="#!" class="nav-link"><span class="pcoded-micon"><i
-                                    class="fas fa-user-secret"></i></span><span class="pcoded-mtext">Quản lí player</span></a>
+                                    class="fas fa-user-secret"></i></span><span class="pcoded-mtext">Quản lí
+                                player</span></a>
                         <ul class="pcoded-submenu">
-                            <li class=""><a href="{{ route('admin.players.index') }}" class="">Danh sách</a></li>
+                            <li class=""><a href="{{ route('admin.players.index') }}" class="">Danh sách</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
+                        class="nav-item pcoded-hasmenu">
+                        <a href="#!" class="nav-link"><span class="pcoded-micon"><i
+                                    class="fas fa-user-secret"></i></span><span class="pcoded-mtext">Quản lí
+                                player</span></a>
+
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="{{ route('admin.players.index') }}" class="">Danh
+                                    sách</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
+                        class="nav-item pcoded-hasmenu">
+                        <a href="{{ route('admin.tocao.index') }}" class="nav-link"><span class="pcoded-micon"><i
+                                    class="feather icon-credit-card"></i></span><span class="pcoded-mtext">Tố cáo
+                                player</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="{{ route('admin.tocao.index') }}" class="">Danh
+                                    sách</a>
+                            </li>
+                            <li class=""><a href="{{ route('admin.tocao.add') }}" class="">Thêm
+                                    mới</a>
+                            </li>
                         </ul>
                     </li>
                 </ul>
