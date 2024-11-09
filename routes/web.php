02a28547f724f6e0def2e409027482bf349e361f
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-11-09 09:43:25 +0700

    tao layout cai dat tai khoan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Client\TaiKhoanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('client.index');
 });
+
+Route::get('tai_khoan/thong_tin_ca_nhan', [TaiKhoanController::class, 'index'])->name('taikhoan.thongtincanhan');
