5381739f2f00e99d6d8a457233c8ab6bcfb9960b
mailanne00 <lanvmph37426@fpt.edu.vn>
2024-11-19 00:06:15 +0700

    list bình luận

--- app/Http/Controllers/Admin/BinhLuanController.php
+++ app/Http/Controllers/Admin/BinhLuanController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BinhLuan;
 use Illuminate\Http\Request;
 
 class BinhLuanController extends Controller
 {
-    //
+    public function index()
+    {
+        $binhLuans = BinhLuan::all();
+        return view('admin.binh-luan.index', compact('binhLuans'));
+    }
 }
