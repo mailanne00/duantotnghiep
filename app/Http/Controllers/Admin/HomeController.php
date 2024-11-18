0b567089d915e079b3f06e6e4eea724cd453d89e
mailanne00 <lanvmph37426@fpt.edu.vn>
2024-11-19 04:24:28 +0700

    thống kê đơn thuê và top 10 user

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -20,6 +20,44 @@ class HomeController extends Controller
              return ($lichSuThue->gio_thue * $lichSuThue->gia_thue * $lichSuThue->nguoiDuocThue->loi_nhuan) / 100;
          });
 
-         return view('admin.index', compact('taiKhoan', 'countPhanQuyen1', 'countPhanQuyen2', 'countRent', 'totalProfit'));
+         $topPlayers = LichSuThue::select('nguoi_duoc_thue', LichSuThue::raw('COUNT(*) as total_rents'))
+             ->groupBy('nguoi_duoc_thue')
+             ->orderBy('total_rents', 'desc')
+             ->with('nguoiDuocThue')
+             ->limit(10)
+             ->get();
+
+         $chartData = $topPlayers->map(function ($player) {
+             return [
+                 'name' => $player->nguoiDuocThue->ten ?? 'Không xác định',
+                 'count' => $player->total_rents,
+             ];
+         })->toArray();
+
+         $rentStatusByMonth = LichSuThue::select(
+             LichSuThue::raw('MONTH(created_at) as month'), 'trang_thai',
+             LichSuThue::raw('COUNT(*) as total')
+         )
+             ->whereYear('created_at', now()->year)
+             ->groupBy('month', 'trang_thai')
+             ->get();
+
+         $rentData = [];
+         foreach ($rentStatusByMonth as $item) {
+             $month = $item->month;
+             $status = $item->trang_thai;
+             $rentData[$month][$status] = $item->total;
+         }
+
+         for ($i = 1; $i <= 12; $i++) {
+             $rentData[$i] = [
+                 0 => $rentData[$i][0] ?? 0,
+                 1 => $rentData[$i][1] ?? 0,
+                 2 => $rentData[$i][2] ?? 0,
+             ];
+         }
+         ksort($rentData);
+
+         return view('admin.index', compact('taiKhoan', 'countPhanQuyen1', 'countPhanQuyen2', 'countRent', 'totalProfit', 'chartData','rentData'));
      }
 }
