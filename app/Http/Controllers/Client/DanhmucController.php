2cf859de81148c7f1efee00c60cb05870057027b
bennqph33983 <bennqph33983@fpt.edu.vn>
2024-11-21 19:32:59 +0700

    controller

--- app/Http/Controllers/Client/DanhmucController.php
+++ app/Http/Controllers/Client/DanhmucController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\DanhMuc;
+use App\Models\DanhMucTaiKhoan;
 
 class DanhmucController extends Controller
 {
     public function index()
     {
         return view('client.danh-muc.index');
+    }
+    public function show(int $id)
+    {
+        $danhMuc = DanhMuc::query()->findOrFail($id);
+        $taiKhoans = DanhMucTaiKhoan::query()->where('danh_muc_id', $id)->get();
+
+        return view('client.danh-muc.show', compact('danhMuc', 'taiKhoans'));
     }
 }
