d83f1cb02f0dbd9462bf6c7d09c542346227ed81
Vdoan2004 <doannvph35275@fpt.edu.vn>
2024-11-29 22:16:58 +0700

    1

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -158,7 +158,7 @@
                     <div class="heading-live-auctions">
                         <h2 class="tf-title pb-22">
                             Hot Players</h2>
-                        <a href="explore-3.html" class="exp style2">XEM </a>
+                        <a href="explore-3.html" class="exp style2">XEM TẤT</a>
                     </div>
                 </div>
                 <div class="col-md-12">
